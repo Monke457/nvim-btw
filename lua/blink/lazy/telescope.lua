17fcf6a47abbe2411142468058bf2c5e27e25669
Beth <beth.williams@bluewin.ch>
2025-01-17 21:32:32 +0100

    goto diagnostics

--- lua/blink/lazy/telescope.lua
+++ lua/blink/lazy/telescope.lua
@@ -1,10 +1,19 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
 vim.keymap.set('n', '<leader>sf', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+--vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
 
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- lua/blink/remap.lua
+++ lua/blink/remap.lua
@@ -6,13 +6,10 @@ vim.keymap.set("i", "kk", "<Esc>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>N", ":rightb vs BufEnter<Enter>")
 vim.keymap.set("n", "<leader>jj", "<C-W>j")
 vim.keymap.set("n", "<leader>kk", "<C-W>k")
 vim.keymap.set("n", "<leader>hh", "<C-W>h")
 vim.keymap.set("n", "<leader>ll", "<C-W>l")
-
-vim.keymap.set("n", "<leader>H", ":rightb vs BufEnter<Enter>:h<Enter><C-W>j:q<Enter>")
 
 vim.keymap.set("n", "<leader>r", "*:%s//")
 vim.keymap.set("v", "<leader>e", ":norm A")
@@ -20,8 +17,13 @@ vim.keymap.set("v", "<leader>w", ":norm I")
 
 vim.keymap.set("n", "<A-p>", "<C-^>")
 
-vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>")
+vim.keymap.set("n", "en", vim.diagnostic.goto_next)
+vim.keymap.set("n", "ep", vim.diagnostic.goto_prev)
 
+--
+--[[ BOILERPLATE ]]--
+--
+--[[ advent of code go boilerplate ]]--
 vim.keymap.set("n", "<leader>maingo", "ipackage main"..
 "<Enter><Enter>import(<Enter>\"advent/internal/pkg/reader\""..
 "<Enter>\"fmt\"<Enter>)<Enter><Enter>"..
@@ -30,9 +32,11 @@ vim.keymap.set("n", "<leader>maingo", "ipackage main"..
 "<Enter><Enter>for _, line := range data {<Enter>}<Esc>"..
 "<S-o>fmt.Println(line)<Esc>{kA<Esc>hi")
 
+--[[ go if err != nil boilerplate ]]--
 vim.keymap.set("n", "<leader>errgo", "oif err != nil {<Enter>}<Esc>"..
 "<S-o>panic(err)<Esc><S-]>")
 
+--[[ bassin html boilerplate ]]--
 vim.keymap.set("n", "<leader>html", "i<DOCTYPE html>"..
 "<Enter><html lang=en><Enter><head><Enter><title></title>"..
 "<Enter><meta charset=\"UTF-8\">"..
